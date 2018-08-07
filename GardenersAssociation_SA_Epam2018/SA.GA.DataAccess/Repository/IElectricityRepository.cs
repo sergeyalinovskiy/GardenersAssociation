054b034bc167463cc2004bf9e896361ff3fdf47d
MINSK\Siarhei_Alinouski <Siarhei_Alinouski@epam.com>
2018-08-07 17:35:51 +0300

    Added dependency references  for Repository

--- GardenersAssociation_SA_Epam2018/SA.GA.DataAccess/Repository/IElectricityRepository.cs
+++ GardenersAssociation_SA_Epam2018/SA.GA.DataAccess/Repository/IElectricityRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using SA.GA.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SA.GA.DataAccess.Repository
 {
-    interface IElectricityRepository
+    public interface IElectricityRepository
     {
+        void Create(Electricity item);
+        void Delete(int id);
+        void Update(Electricity item);
+        Electricity GetById(int id);
+        IEnumerable<Electricity> GetAll();
     }
 }
