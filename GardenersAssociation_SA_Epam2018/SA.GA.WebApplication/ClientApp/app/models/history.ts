549cf642588494e58b27b11aa1cbe8a2583c3119
MINSK\Siarhei_Alinouski <Siarhei_Alinouski@epam.com>
2018-08-09 19:19:10 +0300

    added history ui model

---
+++ GardenersAssociation_SA_Epam2018/SA.GA.WebApplication/ClientApp/app/models/history.ts
@@ -0,0 +1,10 @@
+﻿import { Data } from "@angular/router";
+
+export class Rate {
+    constructor(
+        public id?: number,
+        public UserId?: number,
+        public PlotId?: number,
+        public From?: Data,
+        public To?: Data) { }
+}
