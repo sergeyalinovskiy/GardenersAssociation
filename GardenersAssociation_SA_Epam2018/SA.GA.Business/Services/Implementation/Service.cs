5ceb66fac4353527e5d94311c834333663402f83
MINSK\Siarhei_Alinouski <Siarhei_Alinouski@epam.com>
2018-08-13 20:03:33 +0300

    added genereic Service

---
+++ GardenersAssociation_SA_Epam2018/SA.GA.Business/Services/Implementation/Service.cs
@@ -0,0 +1,30 @@
+﻿using SA.GA.DataAccess.Context.Implementation;
+using SA.GA.DataAccess.Repository;
+using SA.GA.DataAccess.Repository.Implementation;
+namespace SA.GA.Business.Services.Implementation
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+                    
+        public Service()
+        {
+            _repository = new Repository<T>(new AppContext2());
+        }
+            
+        public void Create(T item)
+        {
+            _repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+            
+        public void Update(T item)
+        {
+            _repository.Update(item);
+        }
+    }
+}
