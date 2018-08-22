73a7b650725e37fde5f54c30f4ab9e24b470e3d3
MINSK\Siarhei_Alinouski <Siarhei_Alinouski@epam.com>
2018-08-22 19:01:55 +0300

    Added SummaryTable controller and ViewModel

--- GardenersAssociation_SA_Epam2018/SA.GA.Business/Services/Implementation/ElectricityService.cs
+++ GardenersAssociation_SA_Epam2018/SA.GA.Business/Services/Implementation/ElectricityService.cs
@@ -83,6 +83,5 @@
             rates.Add(rate);
             return rates;
         }
-
     }
 }
---
+++ GardenersAssociation_SA_Epam2018/SA.GA.WebApplication/ViewModels/SummaryTableViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA.GA.WebApplication.ViewModels
+{
+    public class SummaryTableViewModel
+    {
+        public int UserId { get; set; }
+        public double PreviousTestimony { get; set; }
+        public double RecentTestimony { get; set; }
+        public double Consumption { get; set; }
+        public double NecessaryToPay { get; set; }
+       
+    }
+}
